286d624d37f5a3026525cac116c4705bd8d853fd
Felicia Weathers <Fel.Weathers@gmail.com>
2016-05-10 23:48:45 -0400

    Extend Velocity to support kph and mph

--- Extensions.playground/Contents.swift
+++ Extensions.playground/Contents.swift
@@ -5,3 +5,8 @@ import UIKit
 var str = "Hello, Felicia"
 
 typealias Velocity = Double
+extension Velocity {
+    var kph: Velocity { return self * 1.60934 }
+    var mph: Velocity { return self }
+}
+
